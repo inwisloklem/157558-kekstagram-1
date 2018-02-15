60ddd9813cb5e3bfecf052bbe8cf4848e9a73082
inwisloklem <89099109104@mail.ru>
2018-02-15 18:16:27 +0300

    Refactor version colorization

--- src/version.js
+++ src/version.js
@@ -1,28 +1,12 @@
 const {version} = require(`../package.json`);
+require(`colors`);
 
-const VERSION_COLORS = [
-  `red`,
-  `green`,
-  `blue`,
-];
-
-const colorizeBy = (colors) =>
-  (prev, next, ndx) => {
-    prev += next[colors[ndx]];
-    if (colors.length - 1 !== ndx) {
-      prev += `.`;
-    }
-    return prev;
-  };
-
-const colorizedVersion = version
-    .split(`.`)
-    .reduce(colorizeBy(VERSION_COLORS), ``);
+const [major, minor, patch] = version.split(`.`);
 
 module.exports = {
   name: `version`,
   description: `Shows program version`,
   execute() {
-    console.log(`v${colorizedVersion}`);
+    console.log(`v${major.red}.${minor.green}.${patch.blue}`);
   }
 };

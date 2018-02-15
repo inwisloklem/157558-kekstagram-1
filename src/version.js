0e438a3cdadcc8a4b63772fd24eb617802c26260
inwisloklem <89099109104@mail.ru>
2018-02-15 17:48:26 +0300

    Colorize version

--- src/version.js
+++ src/version.js
@@ -1,9 +1,28 @@
 const {version} = require(`../package.json`);
 
+const VERSION_COLORS = [
+  `red`,
+  `green`,
+  `blue`,
+];
+
+const colorizeBy = (colors) =>
+  (prev, next, ndx) => {
+    prev += next[colors[ndx]];
+    if (colors.length - 1 !== ndx) {
+      prev += `.`;
+    }
+    return prev;
+  };
+
+const colorizedVersion = version
+    .split(`.`)
+    .reduce(colorizeBy(VERSION_COLORS), ``);
+
 module.exports = {
   name: `version`,
   description: `Shows program version`,
   execute() {
-    console.log(`v${version}`);
+    console.log(`v${colorizedVersion}`);
   }
 };

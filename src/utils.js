f52ee96f2e683033563e9e7f0fb073a03a9b23de
inwisloklem <89099109104@mail.ru>
2018-02-16 18:12:45 +0300

    Add random function

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,11 @@ const byName = (name) => (array) =>
 const pipe = (...fns) =>
   fns.reduce((f, g) => (...args) => g(f(...args)));
 
+const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * ((max - min) + 1)) + min;
+
 module.exports = {
   byName,
+  getRandomNumber,
   pipe,
 };

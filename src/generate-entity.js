a4c9c63bf36aecd1decbfa7219a166ce7c3b0ae2
inwisloklem <89099109104@mail.ru>
2018-02-18 18:56:57 +0300

    Refactor generateEntity

--- src/generate-entity.js
+++ src/generate-entity.js
@@ -6,42 +6,19 @@ const {
 
 const {
   getRandomNumber,
-  pipe,
+  getRandomArrayElement,
+  getRandomSubarray,
 } = require(`./utils.js`);
-
-const getRandomElement = (randomFn) =>
-  (array) =>
-    () => array[randomFn(0, array.length - 1)];
-
-const getRandomFrom = getRandomElement(getRandomNumber);
-
-const generateBy = (fn) =>
-  (number) => {
-    let result = [];
-
-    for (let max = number; max--;) {
-      result.push(fn());
-    }
-
-    return result;
-  };
-
-const getRandomComment = getRandomFrom(COMMENTS);
-const getRandomEffect = getRandomFrom(EFFECTS);
-const getRandomHashtag = getRandomFrom(HASHTAGS);
-
-const generateComments = pipe(getRandomNumber, generateBy(getRandomComment));
-const generateHashtags = pipe(getRandomNumber, generateBy(getRandomHashtag));
 
 const generateEntity = () => {
   return {
     url: `https://picsum.photos/600/?random`,
     scale: getRandomNumber(0, 100),
-    effect: getRandomEffect(),
-    hashtags: generateHashtags(0, 5),
-    description: getRandomComment(),
+    effect: getRandomArrayElement(EFFECTS),
+    hashtags: getRandomSubarray(HASHTAGS, getRandomNumber(0, 5)),
+    description: getRandomArrayElement(COMMENTS),
     likes: getRandomNumber(0, 1000),
-    comments: generateComments(0, 10),
+    comments: getRandomSubarray(COMMENTS, getRandomNumber(0, 10)),
   };
 };
 

e30e788c95aa3118956b2ec21e5d565d2a4661d3
inwisloklem <89099109104@mail.ru>
2018-03-07 10:39:56 +0300

    Rename assert functions

--- src/post-scheme.js
+++ src/post-scheme.js
@@ -1,59 +1,59 @@
 const {EFFECTS} = require(`./data.js`);
 
 const {
-  combine,
-  everyTextLength,
-  everyWordIsUnique,
-  everyWordStartsWith,
-  isGreaterThanOrEqual,
-  isImage,
-  isInRange,
-  isInteger,
-  isOneOf,
-  maxWords,
-  textLength,
+  combineAsserts,
+  checkEveryTextLength,
+  checkEveryWordIsUnique,
+  checkEveryWordStartsWith,
+  checkGreaterThanOrEqual,
+  checkImage,
+  checkInRange,
+  checkInteger,
+  checkOneOf,
+  checkMaxWords,
+  checkTextLength,
 } = require(`./asserts.js`);
 
 const scheme = {
   date: {
     required: true,
     asserts: [
-      isInteger,
-      isGreaterThanOrEqual(0),
+      checkInteger,
+      checkGreaterThanOrEqual(0),
     ],
   },
   description: {
     asserts: [
-      textLength(0, 140),
+      checkTextLength(0, 140),
     ],
   },
   effect: {
     required: true,
     asserts: [
-      isOneOf(EFFECTS),
+      checkOneOf(EFFECTS),
     ],
   },
   filename: {
     required: true,
     asserts: [
-      isImage,
+      checkImage,
     ],
   },
   hashtags: {
     asserts: [
-      combine(
-          everyTextLength(0, 20),
-          everyWordIsUnique,
-          everyWordStartsWith(`#`),
-          maxWords(5),
+      combineAsserts(
+          checkEveryTextLength(0, 20),
+          checkEveryWordIsUnique,
+          checkEveryWordStartsWith(`#`),
+          checkMaxWords(5),
       ),
     ],
   },
   scale: {
     required: true,
     asserts: [
-      isInteger,
-      isInRange(0, 100),
+      checkInteger,
+      checkInRange(0, 100),
     ],
   },
 };

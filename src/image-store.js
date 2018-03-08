1b9216001c928ec249eb0a81003ed1f4afe20a81
inwisloklem <89099109104@mail.ru>
2018-03-08 18:43:23 +0300

    Add image store module

---
+++ src/image-store.js
@@ -0,0 +1,52 @@
+const mongodb = require(`mongodb`);
+const db = require(`./database.js`);
+
+class ImageStore {
+  async getBucket() {
+    if (this._bucket) {
+      return this._bucket;
+    }
+
+    const dBase = await db;
+
+    if (!this._bucket) {
+      this._bucket = new mongodb.GridFSBucket(dBase, {
+        chunkSizeBytes: 1024 * 1024,
+        bucketName: `images`
+      });
+    }
+
+    return this._bucket;
+  }
+
+  async get(filename) {
+    const bucket = await this.getBucket();
+
+    const results = await (bucket)
+        .find({filename})
+        .toArray();
+
+    const entity = results[0];
+
+    if (!entity) {
+      return null;
+    }
+
+    return {
+      info: entity,
+      stream: bucket.openDownloadStreamByName(filename),
+    };
+  }
+
+  async save(filename, stream) {
+    const bucket = await this.getBucket();
+
+    return new Promise((success, fail) => {
+      stream
+          .pipe(bucket.openUploadStream(filename))
+          .on(`error`, fail).on(`finish`, success);
+    });
+  }
+}
+
+module.exports = new ImageStore();

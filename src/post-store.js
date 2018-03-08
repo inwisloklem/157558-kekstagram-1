88fa7792cdef8642696a8d10dad16cc540a01df7
inwisloklem <89099109104@mail.ru>
2018-03-08 18:43:43 +0300

    Add posts store module

---
+++ src/post-store.js
@@ -0,0 +1,37 @@
+const db = require(`./database.js`);
+const {log} = require(`./utils.js`);
+
+const setupCollection = async () => {
+  const dBase = await db;
+
+  const collection = dBase.collection(`posts`);
+  collection.createIndex({date: -1}, {unique: true});
+
+  return collection;
+};
+
+class PostStore {
+  constructor(collection) {
+    this.collection = collection;
+  }
+
+  async getAllPosts() {
+    return (await this.collection)
+        .find();
+  }
+
+  async getPostByQuery(query) {
+    return (await this.collection)
+        .findOne(query);
+  }
+
+  async savePost(data) {
+    return (await this.collection)
+        .insertOne(data);
+  }
+}
+
+module.exports = new PostStore(
+    setupCollection()
+        .catch((e) => log({message: `Failed to setup collecton: ${e}`, type: `error`}))
+);

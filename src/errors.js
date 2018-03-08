23dd5f4b19919f92724218250abeded84797a6e3
inwisloklem <89099109104@mail.ru>
2018-03-08 18:43:13 +0300

    Add 500 error

--- src/errors.js
+++ src/errors.js
@@ -7,6 +7,10 @@ const Errors = {
     error: `Not Found`,
     errorMessage: `Server cannot find requested resource.`,
   },
+  INTERNAL_SERVER_ERROR: {
+    error: `Internal Server Error`,
+    errorMessage: `Server has encountered a situation it doesn't know how to handle.`,
+  },
   NOT_IMPLEMENTED: {
     error: `Not Implemented`,
     errorMessage: `Request method is not supported by the server and cannot be handled.`,

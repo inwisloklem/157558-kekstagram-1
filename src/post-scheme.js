6c417b3673c97bfe5891bc5aa20e83a05f3400e6
inwisloklem <89099109104@mail.ru>
2018-03-09 19:09:59 +0300

    Remove post settings from scheme

--- src/post-scheme.js
+++ src/post-scheme.js
@@ -1,25 +1,6 @@
 const {EFFECTS} = require(`./data.js`);
 
-const Settings = {
-  HASHTAG: `#`,
-  DATE_FROM: -999999999,
-  DATE_TO: 999999999,
-  MIN_DATE: 0,
-  MAX_COMMENTS: 10,
-  MIN_COMMENTS: 0,
-  MAX_COMMENT_LENGTH: 140,
-  MIN_COMMENT_LENGTH: 0,
-  MAX_DESC_LENGTH: 140,
-  MIN_DESC_LENGTH: 0,
-  MAX_HASHTAG_LENGTH: 20,
-  MIN_HASHTAG_LENGTH: 0,
-  MAX_HASHTAGS: 5,
-  MIN_HASHTAGS: 0,
-  MAX_LIKES: 1000,
-  MIN_LIKES: 0,
-  MAX_SCALE: 100,
-  MIN_SCALE: 0,
-};
+const Settings = require(`./post-settings.js`);
 
 const {
   combineAsserts,
@@ -79,7 +60,4 @@ const scheme = {
   },
 };
 
-module.exports = {
-  scheme,
-  Settings,
-};
+module.exports = scheme;

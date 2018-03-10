ff1e425269bd3ec46c702b10102edc3f7a5be8f8
inwisloklem <89099109104@mail.ru>
2018-03-10 20:51:55 +0300

    Add and configure logger module

---
+++ src/server/logger.js
@@ -0,0 +1,28 @@
+const winston = require(`winston`);
+
+const {
+  combine,
+  timestamp,
+  printf,
+} = winston.format;
+
+const logger = winston.createLogger({
+  level: process.env.SERVER_LOG_LEVEL || `info`,
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({filename: `error.log`, level: `error`}),
+    new winston.transports.File({filename: `combined.log`})
+  ]
+});
+
+if (process.env.NODE_ENV !== `production`) {
+  logger.add(new winston.transports.Console({
+    format: combine(
+        timestamp(),
+        printf((info) =>
+          `${info.level.toUpperCase().cyan} ${info.timestamp.gray} ${info.message}`),
+    ),
+  }));
+}
+
+module.exports = logger;

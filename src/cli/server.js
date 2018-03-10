f7b7bf720cb6c95fe211f833f5fddb89f8fdb4b9
inwisloklem <89099109104@mail.ru>
2018-03-10 09:55:55 +0300

    Fix server module freezes the app

--- src/cli/server.js
+++ src/cli/server.js
@@ -1,29 +1,37 @@
 const express = require(`express`);
-const app = express();
 
 const {log} = require(`../utils`);
 
-const postStore = require(`../server/post-store`);
-const imageStore = require(`../server/image-store`);
+const PostStore = require(`../server/post-store`);
+const ImageStore = require(`../server/image-store`);
 
-const router = require(`../server/post-routes`)(postStore, imageStore);
+const initMainDb = require(`../server/database`);
+const setupCollection = require(`../server/setup`);
 
-const {
-  SERVER_HOST,
-  SERVER_PORT,
-} = require(`./config`);
-
-app
-    .set(`host`, SERVER_HOST)
-    .set(`port`, process.argv[3] || SERVER_PORT)
-
-    .use(`/api/posts`, router)
-    .use(express.static(`static`));
+const Config = require(`../config`);
 
 module.exports = {
   name: `server`,
   description: `Starts local server`,
   execute() {
+    const app = express();
+
+    const postStore = new PostStore(
+        setupCollection(initMainDb)
+            .catch((e) => log({message: `Failed to setup collecton: ${e}`, type: `error`}))
+    );
+
+    const imageStore = new ImageStore();
+
+    const router = require(`../server/post-routes`)(postStore, imageStore);
+
+    app
+        .set(`host`, Config.SERVER_HOST)
+        .set(`port`, process.argv[3] || Config.SERVER_PORT)
+
+        .use(`/api/posts`, router)
+        .use(express.static(`static`));
+
     app.listen(app.get(`port`), () => log({
       message: `Local server starts now at ${app.get(`host`)}:${app.get(`port`)}.`,
       type: `text`,

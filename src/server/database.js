cc91aa71ff98f7ad056313bbf632f284cc89c651
inwisloklem <89099109104@mail.ru>
2018-03-11 13:40:43 +0300

    Add logger info

--- src/server/database.js
+++ src/server/database.js
@@ -9,9 +9,13 @@ const url = process.env.DB_HOST || DB_HOST;
 
 const initMainDb = () => MongoClient
     .connect(url)
-    .then((client) => client.db(`kekstagram`))
+    .then(
+        (client) => {
+          logger.info(`Connected to database.`);
+          return client.db(`kekstagram`);
+        })
     .catch((e) => {
-      logger.error(`Fails to connect to database`, e);
+      logger.error(`Failed to connect to database.`, {details: {error: e}});
       process.exit(1);
     });
 

86defd713f0e4d6caa804df98a8755df87c86dde
inwisloklem <89099109104@mail.ru>
2018-03-11 13:40:06 +0300

    Improve logger

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,4 +1,5 @@
 const winston = require(`winston`);
+const {prettyPrint} = require(`../utils`);
 
 require(`dotenv`).config();
 
@@ -8,23 +9,29 @@ const {
   printf,
 } = winston.format;
 
+const format = combine(
+    timestamp(),
+    printf((info) => {
+      const message = `${info.level.toUpperCase().cyan} ${info.timestamp} ${info.message}`;
+      return info.details ? `${message}\n${prettyPrint(info.details).grey}` : message;
+    })
+);
+
 const logger = winston.createLogger({
   level: process.env.SERVER_LOG_LEVEL.toLowerCase() || `info`,
-  format: winston.format.json(),
+  format: printf((info) => `${prettyPrint(info)},`),
   transports: [
     new winston.transports.File({filename: `error.log`, level: `error`}),
-    new winston.transports.File({filename: `combined.log`})
+    new winston.transports.File({filename: `combined.log`, level: `silly`}),
+    new winston.transports.File({filename: `combined.log`, level: `info`}),
   ]
 });
 
+logger.silly = (...args) =>
+  logger.log(`silly`, ...args);
+
 if (process.env.NODE_ENV !== `production`) {
-  logger.add(new winston.transports.Console({
-    format: combine(
-        timestamp(),
-        printf((info) =>
-          `${info.level.toUpperCase().cyan} ${info.timestamp.gray} ${info.message}`),
-    ),
-  }));
+  logger.add(new winston.transports.Console({format}));
 }
 
 module.exports = logger;
